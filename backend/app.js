4ea19c67c6ff09276f01333b312a656a150d3146
bbowcutt <brookebowcutt1@gmail.com>
2023-04-12 08:12:58 -0600

    switching over to using routers all in app.js

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,9 @@ const {storeRouter} = require('./routes/stores.js');
 app.use(cors());
 app.use(express.json());
 
+const storeRouter = Router(); 
+const itemRouter = Router(); 
+
 const dbUrl = 'mongodb://127.0.0.1:27017/stores';
 mongoose.connect(dbUrl);
 
@@ -20,11 +23,61 @@ db.once('open', function() {
 });
 
 
-
 app.get('/', async(req, res) => {
     const stores = await Store.find();
     res.send( stores )
 }); 
+
+storeRouter.get('/stores', async (req, res) => {
+  const stores = await Store.find();
+  res.send(stores);
+});
+
+storeRouter.get('stores/:storeId', async (req, res) => {
+  const storeId = req.params.storeId;
+  try {
+    const store = await Store.findOne({ _id: storeId });
+    console.log(store);
+    if (store === null) {
+      res.status(404);
+      res.json({
+        status: 404,
+        message: 'not found',
+      });
+      return;
+    }
+    // The MongoDB driver returns data as JavaScript objects, so we don't need to parse them to pass them to the `json` method of
+    // Express' `Response` object
+    res.json(store);
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.send('');
+  }
+});
+
+storeRouter.post("/stores/new", async (req, res) => {
+  const requestBody = req.body;
+  requestBody._id = uuidv4();
+
+  try {
+    const result = await new Store(requestBody).save();
+    console.log(result);
+    res.status(201);
+    res.json({
+      status: 201,
+      message: 'created',
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.json({
+      status: 500,
+      message: e,
+    });
+  }
+}); 
+
 
 app.use('stores', storeRouter); 
 
--- backend/routes/stores.js
+++ backend/routes/stores.js
@@ -1,61 +1,61 @@
-import { v4 as uuidv4 } from 'uuid';
-import { Router } from 'express';
-import * as fs from 'node:fs/promises';
-import {Store} from './models.js'; 
+// const uuidv4 = require('uuid');
+// const { Router } = require('express');
+// //import * as fs from 'node:fs/promises';
+// const {Store} =  require('../models.js'); 
 
-const storeRouter = Router(); 
+// const storeRouter = Router(); 
 
-blogRouter.get('/', async (req, res) => {
-    const stores = await Store.find();
-    res.send(stores);
-  });
+// storeRouter.get('/', async (req, res) => {
+//     const stores = await Store.find();
+//     res.send(stores);
+//   });
 
-blogRouter.get('stores/:storeId', async (req, res) => {
-    const storeId = req.params.storeId;
-    try {
-      const store = await Store.findOne({ _id: storeId });
-      console.log(store);
-      if (store === null) {
-        res.status(404);
-        res.json({
-          status: 404,
-          message: 'not found',
-        });
-        return;
-      }
-      // The MongoDB driver returns data as JavaScript objects, so we don't need to parse them to pass them to the `json` method of
-      // Express' `Response` object
-      res.json(store);
-    } catch (e) {
-      console.log(e);
-      res.status(500);
-      res.send('');
-    }
-  });
+// storeRouter.get('stores/:storeId', async (req, res) => {
+//     const storeId = req.params.storeId;
+//     try {
+//       const store = await Store.findOne({ _id: storeId });
+//       console.log(store);
+//       if (store === null) {
+//         res.status(404);
+//         res.json({
+//           status: 404,
+//           message: 'not found',
+//         });
+//         return;
+//       }
+//       // The MongoDB driver returns data as JavaScript objects, so we don't need to parse them to pass them to the `json` method of
+//       // Express' `Response` object
+//       res.json(store);
+//     } catch (e) {
+//       console.log(e);
+//       res.status(500);
+//       res.send('');
+//     }
+//   });
 
 
-storeRouter.post("/stores/new", async (req, res) => {
-    const requestBody = req.body;
-    requestBody._id = uuidv4();
+// storeRouter.post("/stores/new", async (req, res) => {
+//     const requestBody = req.body;
+//     requestBody._id = uuidv4();
   
-    try {
-      const result = await new Store(requestBody).save();
-      console.log(result);
-      res.status(201);
-      res.json({
-        status: 201,
-        message: 'created',
-      });
-    } catch (e) {
-      console.log(e);
-      res.status(500);
-      res.json({
-        status: 500,
-        message: e,
-      });
-    }
-  })
+//     try {
+//       const result = await new Store(requestBody).save();
+//       console.log(result);
+//       res.status(201);
+//       res.json({
+//         status: 201,
+//         message: 'created',
+//       });
+//     } catch (e) {
+//       console.log(e);
+//       res.status(500);
+//       res.json({
+//         status: 500,
+//         message: e,
+//       });
+//     }
+//   }); 
 
  
 
-export default storeRouter; 
+// export default storeRouter; 
